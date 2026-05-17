d338df604c6b03db83b6586a4156aa08473ef214
Ed Martins <contact.edmartins@gmail.com>
2026-05-17 14:39:00 -0300

    chore: rubocop pass

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
-  resources :forecasts, only: [:new, :create]
+  resources :forecasts, only: [ :new, :create ]
   root "forecasts#new"
 end
